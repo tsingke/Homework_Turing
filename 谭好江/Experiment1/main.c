0453a8f57206b24cf7a1319e486fde54f7b051e5
谭好江 <201711010336@stu.sdnu.edu.cn>
2018-09-23 17:07:02 +0800

    add a new file

---
+++ 谭好江/Experiment1/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "score.h"
+//#include"score.c"
+
+
+
+int main()
+{
+	int N = 0;        //学生总数
+	SS  *pstu = NULL;    //结构体数组指针
+	 //读取数据信息
+
+		/*主菜单*/
+
+		printf("\n\n******************************\n");
+		printf("       学生成绩管理分析系统        \n");
+		printf("******************************\n\n");
+
+		printf("读取所有学生信息\n");
+            pstu = readDataFromFile(&N);
+		
+		
+		printf("计算学生成绩排名\n");
+            calcuScore(pstu, N);
+			sortScore(pstu, N);
+			printOut(pstu, N);
+		
+			
+		printf("输入学号查询成绩\n");
+            searchScore(pstu);
+		
+		printf("统计出该课程成绩的均值和方差\n");
+            average(pstu, N);
+			variance(pstu, N);
+		
+
+
+
+	free(pstu);//释放动态内存空间
+
+	printf("\n");
+	system("pause");
+
+
+	return 0;
+}
