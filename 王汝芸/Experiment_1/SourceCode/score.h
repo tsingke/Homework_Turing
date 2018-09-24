8317912f545e003360986816df6c8b111381b0a2
RuYunW <42913632+RuYunW@users.noreply.github.com>
2018-09-24 19:46:06 +0800

    王汝芸3.0

--- 王汝芸/Experiment_1/SourceCode/score.h
+++ 王汝芸/Experiment_1/SourceCode/score.h
@@ -22,6 +22,7 @@ typedef struct //学号 姓名 平时成绩 实验成绩 期末成绩
 	float expScore;  //实验成绩
 	float finalScore;   //期末成绩
 	float generalScore; //总评成绩
+	int rank;   //总成绩排名
 	
 }SS;
 
@@ -29,22 +30,18 @@ typedef struct //学号 姓名 平时成绩 实验成绩 期末成绩
 /*---------------函数声明-------------------*/
 
 //1.读取学生基本数据
-//void readData(SS stu[], int N);
+
 SS* readDataFromFile(int *N);
 
 //2.计算N个学生的总评成绩
 void calcuScore(SS stu[], int N);
 
-
 //3.根据总评成绩排名
 void sortScore(SS stu[], int N);
-
 
 //4.按照一定的格式输出N个学生的完整信息
 void printOut(SS stu[], int N);
 
-
- 
 
 #endif
 
