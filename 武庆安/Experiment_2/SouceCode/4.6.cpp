8bb124b14bfbc22c16674c4194048104e147a08d
WQA <201711010304@stu.sdnu.edu.cn>
2018-09-27 21:27:19 +0800

    my experiment_2

---
+++ 武庆安/Experiment_2/SouceCode/4.6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+void fun(int x, int &y)
+{
+	x += y;
+	y += x;
+}
+
+int main()
+{
+	int x = 5, y = 10;
+	fun(x, y);
+	fun(y, x);
+	cout << "x=" << x << ",y=" << y << endl;
+	system("pause");
+	return 0;
+}
