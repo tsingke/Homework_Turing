37cff5d294f6362af9c993584d3d78c18b6aacd9
Tsaivv <1041953451@qq.com>
2018-09-28 16:09:59 +0800

    git commit -m

---
+++ 祝 廷 薇/Experiment_2/5.2.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#define Pi 3.14
+int main()
+{
+double r;
+double length,area;
+cin>>r;
+length=2*Pi*r;
+area=Pi*r*r;
+cout<<"length="<<length<<"area="<<area<<endl;
+return 0;
+}
