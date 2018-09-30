bfae3c054da3e000879a70ad4f57efee7520a5d5
张艳秋 <2385361077@qq.com>
2018-09-30 17:57:50 +0800

    third

---
+++ 张艳秋/experiment 3/2.8.1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+#define Multply(x,y) x*y
+int main()
+{
+	int a = Multply(3 + 4, 2 + 3);
+	cout << "a=" << a << endl;
+	
+	system("pause");
+	return 0;
+	
+}
+
