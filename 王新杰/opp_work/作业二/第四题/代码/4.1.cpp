af0a675e669ab7fa25f397b3b5d39a05b3f8443e
王新杰 <w963156517@gmail.com>
2018-10-03 16:04:42 +0800

    第二次上机作业

---
+++ 王新杰/opp_work/作业二/第四题/代码/4.1.cpp
@@ -0,0 +1,15 @@
+//answer2_4_1.cpp
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	char ch;
+	cin >> a >> ch >> b >> c;
+	cout << a << endl << ch << endl << b << endl << c<<endl;
+	system("pause");
+	return 0;
+
+}
