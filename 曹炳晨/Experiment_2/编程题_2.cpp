51f61c586ae9c3381421183c19df5b22d52af5dd
oldCbc <1005784152@qq.com>
2018-10-08 15:39:38 +0800

    work

---
+++ 曹炳晨/Experiment_2/编程题_2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+const int PI = 3.1415926;
+int main()
+{
+	int r, s, l;
+	cin >> r;
+	cout << "面积为：" << PI * r * r << endl;
+	cout << "周长为:" << PI * r * 2 << endl;
+	return 0;
+}
