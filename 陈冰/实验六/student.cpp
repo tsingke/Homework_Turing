5446b756171c250cd2fec3966561a06efe244c55
dreamm2776181586 <l>
2018-11-24 15:19:58 +0800

    第七次提交作业

---
+++ 陈冰/实验六/student.cpp
@@ -0,0 +1,90 @@
+#include"student.h"
+
+Student::Student()
+{
+	name = NULL;
+	age = 0;
+}
+
+Student::Student(char *na, char *id, char *num, char *spec, int ag)
+{
+	if (na)
+	{
+		name = new char[strlen(na) + 1];
+		strcpy(name, na);
+	}
+	strcpy(ID, id);
+	strcpy(number, num);
+	strcpy(speciality, spec);
+	age = ag;
+}
+
+Student::Student(const Student &per)
+{
+	if (per.name)
+	{
+		name = new char[strlen(per.name) + 1];
+		strcpy(name, per.name);
+	}
+	strcpy(ID, per.ID);
+	strcpy(number, per.number);
+	strcpy(speciality, per.speciality);
+	age = per.age;
+}
+
+Student::~Student()
+{
+	if (name)
+		delete[]name;
+}
+
+char* Student::GetName()
+{
+	return name;
+}
+
+char* Student::GetID()
+{
+	return ID;
+}
+
+int Student::GetAge()
+{
+	return age;
+}
+
+char* Student::GetNumber()
+{	return number;}
+
+char*Student::GetSpec()
+{	return speciality;}
+
+void Student::Display()
+{
+	cout << "姓名：" << name << endl;
+	cout << "身份证号：" << ID << endl;
+	cout << "学号：" << number << endl;
+	cout << "专业：" << speciality << endl;
+	cout << "年龄" << age << endl << endl;
+}
+
+void Student::Input()
+{
+	char na[10];
+	cout << "请输入姓名：";
+	cin >> na;
+	if (name)
+		delete []name;
+	name = new char[strlen(na) + 1];
+	strcpy( name, na );
+	cout << "请输入身份证号：";
+	cin >> ID;
+	cout << "请输入年龄：";
+	cin >> age;
+	cout << "请输入专业：";
+	cin >> speciality;
+	cout << "请输入学号：";
+	cin >> number;
+}
+
+
---
+++ 陈冰/实验六/student.h
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+#ifndef _STUDENT
+#define _STUDENT
+#include<iostream>
+#include<string>
+using namespace std;
+#define SIZE 80
+class Student
+{
+	char *name;
+	char ID[19];
+	char number[10];
+	char speciality[20];
+	int age;
+public:
+	Student();
+	Student(char*na, char*id, char*num, char*spec, int ag);
+	Student(const Student &per);
+	~Student();
+	char*GetName();
+	char*GetID();
+	char*GetNumber();
+	char*GetSpec();
+	int GetAge();
+	void Display();
+	void Input();
+};
+#endif
+
+
+
